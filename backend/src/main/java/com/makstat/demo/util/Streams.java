230f4f3e846f8c0f9085101048ee4c8993f71200
Blend Hamiti <b.hamiti.9@gmail.com>
2021-04-28 19:54:28 +0200

    throw exceptions in employeeCount and categories when arg is not valid

---
+++ backend/src/main/java/com/makstat/demo/util/Streams.java
@@ -0,0 +1,14 @@
+package com.makstat.demo.util;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Streams {
+
+    public static <T> Stream<T> asStream(final List<T> list) {
+    return Optional.ofNullable(list)
+            .map(List::stream)
+            .orElseGet(Stream::empty);
+    }
+}
