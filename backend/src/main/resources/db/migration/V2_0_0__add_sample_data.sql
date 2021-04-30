2b578440632e6b1c3ac7050706a22f42f3ec12f7
Blend Hamiti <b.hamiti.9@gmail.com>
2021-05-01 00:30:42 +0200

    added desc to Home controller and updated db migration files

--- backend/src/main/resources/db/migration/V2_0_0__add_sample_data.sql
+++ backend/src/main/resources/db/migration/V2_0_0__add_sample_data.sql
@@ -2,7 +2,8 @@
 
 INSERT INTO "category" ("id", "name") VALUES
 (1,	'sector'),
-(2,	'ageGroup');
+(2,	'ageGroup'),
+(3,	'wageGroup');
 
 -- populate public.sub_category
 
@@ -10,7 +11,9 @@ INSERT INTO "sub_category" ("id", "name", "category_id") VALUES
 (1,	'agriculture',	1),
 (2,	'energy',	1),
 (3,	'18-22',	2),
-(4,	'23-26',	2);
+(4,	'23-26',	2),
+(5,	'6000-12000',	3),
+(6,	'13000-18000',	3);
 
 -- populate public.employee_count
 
@@ -31,3 +34,15 @@ INSERT INTO "employee_count" ("id", "count", "sex", "year", "sub_category_id") V
 (14,	19000,	'1',	2010,	4),
 (15,	20000,	'0',	2011,	4),
 (16,	33000,	'1',	2011,	4);
+
+-- populate public.wage_distribution
+
+INSERT INTO "wage_distribution" ("id", "count", "year", "sub_category_id", "wage_group_id") VALUES
+(1,	133,	2010,	1,	5),
+(2,	245,	2010,	1,	6),
+(3,	456,	2011,	1,	5),
+(4,	564,	2011,	1,	6),
+(5,	321,	2010,	2,	5),
+(6,	231,	2010,	2,	6),
+(7,	321,	2011,	2,	5),
+(8,	546,	2011,	2,	6);
